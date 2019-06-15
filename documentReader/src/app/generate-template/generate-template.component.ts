9429162365da2a69b63a5e4ef4ef556a6747ad65
Vaishali Poria <poriavaishalli@gmail.com>
2019-06-15 15:51:04 +0530

    Bug fix

--- documentReader/src/app/generate-template/generate-template.component.ts
+++ documentReader/src/app/generate-template/generate-template.component.ts
@@ -6,10 +6,14 @@ import { HomeService } from '../home/home.service';
   templateUrl: './generate-template.component.html',
   styleUrls: ['./generate-template.component.scss']
 })
-export class GenerateTemplateComponent implements OnInit,AfterViewInit {
+export class GenerateTemplateComponent implements OnInit, AfterViewInit {
 
   lblList = [];
+  templateId: number;
+  barcode: string;
   data = [];
+  flag = true;
+  respData = {templateId: null, barcode: null, data: null};
   bufferX = 8;
   bufferY = 8;
   activeSlideIndex = 0;
@@ -30,13 +34,46 @@ export class GenerateTemplateComponent implements OnInit,AfterViewInit {
 
   ngOnInit() {
     this.homeService.uploadBehavior.subscribe(res => {
-      console.log(res);
+      if (this.flag) {
+        console.log(res);
+        if (res !== null){
+          //this.templateId = res.data.templateId;
+          //this.validateBarcode(res);
+        }
+        this.flag = false;
+      }
     });
   }
   ngAfterViewInit(): void{
     this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
   }
+/*
+  validateBarcode(res) {
+    if (!res.barcode){
+      this.barcode = prompt('Enter Barcode: ');
+    }
+    this.setUpImage(res);
+  }
 
+  setUpImage(res) {
+    let eleDiv = document.getElementById('image-div');
+    let eleCan = document.getElementById('myCanvas');
+    eleDiv.style.backgroundColor = 'red';
+    console.log('1', eleDiv.style.height, eleDiv.style.width, res.data.images[0].height);
+    eleDiv.style.height = eleCan.style.height = res.data.images[0].height;
+    console.log('2', eleDiv.style.height, eleDiv.style.width, res.data.images[0].height);
+    eleDiv.style.width = eleCan.style.width = res.data.images[0].width;
+    console.log('3', eleDiv.style.height, eleDiv.style.width, res.data.images[0].height);
+    eleDiv.style.backgroundImage = res.data.images[0].imageUrl;
+    console.log(eleDiv.style.height, eleDiv.style.width);
+  }
+
+  sendResp() {
+    this.respData.templateId = this.templateId;
+    this.respData.barcode = this.barcode;
+    this.respData.data = this.data;
+  }
+*/
   InitialXY(e) {
     this.ax = e.clientX;
     this.ay = e.clientY;
@@ -55,14 +92,18 @@ export class GenerateTemplateComponent implements OnInit,AfterViewInit {
     const temp = { lblNum: null, lblText: null, pageNum: null, isPresent: null, xStart: null, xEnd: null, yStart: null, yEnd: null };
     const tempdata = { label: null, pageNo: null, startX: null, startY: null, endX: null, endY: null };
     temp.lblNum = this.lblCounter++;
+    this.barcode = "save labl details";
     temp.lblText = tempdata.label = lblText;
     temp.xStart = tempdata.startX = this.ax;
     temp.yStart = tempdata.startY = this.ay;
     temp.xEnd = tempdata.endX = this.cx;
     temp.yEnd = tempdata.endY = this.cy;
     temp.pageNum = 1;
+    this.barcode='push lbl';
+    console.log(temp);
     this.lblList.push(temp);
-    this.data.push(tempdata);
+    console.log(this.lblList);
+    //this.data.push(tempdata);
     this.drawRect(temp);
   }
   drawRect(lbl) {
@@ -71,6 +112,7 @@ export class GenerateTemplateComponent implements OnInit,AfterViewInit {
     ctx.strokeStyle = '#FF0000';
     ctx.rect(lbl.xStart, lbl.yStart, lbl.xEnd - lbl.xStart, lbl.yEnd - lbl.yStart);
     ctx.stroke();
+    this.barcode='stroke';
     ctx.strokeText(lbl.lblNum, lbl.xEnd , lbl.yEnd );
   }
 }
